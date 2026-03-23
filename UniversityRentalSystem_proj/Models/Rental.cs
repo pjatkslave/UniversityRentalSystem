eda072d785ef404372a7c8948593e0c7b6e91e37
pjatkslave <s32815@pjwstk.edu.pl>
2026-03-23 20:46:51 +0100

    Added Rental class

---
+++ UniversityRentalSystem_proj/Models/Rental.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityRentalSystem_proj.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public User Renter { get; set; }
+        public Equipment RentedEquipment { get; set; }
+        public DateTime RentalDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public decimal Penalty { get; set; }
+        public Rental(int id, User renter, Equipment rentedEquipment, DateTime rentalDateб, int rentalDays)
+        {
+            Id = id;
+            Renter = renter;
+            RentedEquipment = rentedEquipment;
+            ReturnDate = RentalDate.AddDays(rentalDays);
+        }
+    }
+}
