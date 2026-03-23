8b9d341656fca8db3f14e18257962a5e9ffe86db
pjatkslave <s32815@pjwstk.edu.pl>
2026-03-23 18:28:38 +0100

    Initial project structure and base Equipment class

---
+++ UniversityRentalSystem_proj/Models/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityRentalSystem_proj.Models
+{
+    public abstract class Equipment
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsAvailable { get; set; } = true;
+
+        protected Equipment(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public abstract string GetInfo();
+    }
+}
