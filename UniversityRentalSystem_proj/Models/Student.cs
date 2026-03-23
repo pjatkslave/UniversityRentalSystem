1d59a8e0534577a6d28decf564f17a3752ac8610
pjatkslave <s32815@pjwstk.edu.pl>
2026-03-23 20:32:55 +0100

    Added User hierarchy

---
+++ UniversityRentalSystem_proj/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityRentalSystem_proj.Models
+{
+    internal class Student : User
+    {
+        public override int Renal_Limit => 2;
+        public Student(int id, string first_Name, string last_Name) : base(id, first_Name, last_Name, 2){}
+    }
+}
---
+++ UniversityRentalSystem_proj/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityRentalSystem_proj.Models
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public abstract int Renal_Limit { get; }
+    protected User(int id, string first_Name, string last_Name, int renal_Limit)
+        {
+            Id = id;
+            First_Name = first_Name;
+            Last_Name = last_Name;
+        }
+    }
+}
